666a596de1492887753ccd4843083b91343479d8
soubhagya <soubhagya@recruitmaze.com>
2018-12-30 20:39:43 +0530

    fixing errors

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -1,5 +1,5 @@
 import os
-from whitenoise import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
